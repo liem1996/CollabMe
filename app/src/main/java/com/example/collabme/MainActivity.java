70265dda5c3d6e3d5490d9e318fc8153a9b9ddec
amit ben basat <amitbasat124@gmail.com>
2022-03-09 13:13:03 +0200

    adding the social media page in full

--- app/src/main/java/com/example/collabme/MainActivity.java
+++ app/src/main/java/com/example/collabme/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_login);
+        setContentView(R.layout.fragment_socialmedia);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();

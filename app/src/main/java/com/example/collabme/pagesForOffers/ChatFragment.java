f88f29d491c4bdef6dcdd9d60d674e44f125ad9b
eli <eliyahu280@gmail.com>
2022-04-07 17:49:20 +0300

    adding google pay payment

--- app/src/main/java/com/example/collabme/pagesForOffers/ChatFragment.java
+++ app/src/main/java/com/example/collabme/pagesForOffers/ChatFragment.java
@@ -3,14 +3,18 @@ package com.example.collabme.pagesForOffers;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.collabme.R;
 
 public class ChatFragment extends Fragment {
+
+    Button tmpPayment;
 
     public ChatFragment() {
         // Required empty public constructor
@@ -20,6 +24,17 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        tmpPayment = view.findViewById(R.id.tmp_payment_btn);
+
+        tmpPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Navigation.findNavController(view).navigate(ChatFragmentDirections.actionChatFragmentToPaymentFragment());
+            }
+        });
+        return view;
     }
 }

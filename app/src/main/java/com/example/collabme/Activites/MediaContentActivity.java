409e27981bd09d13d3c0302df283c92e2b48218e
Liem Mazal <liemmazal5@gmail.com>
2022-04-09 15:37:22 +0300

    adding the media content activity

---
+++ app/src/main/java/com/example/collabme/Activites/MediaContentActivity.java
@@ -0,0 +1,15 @@
+package com.example.collabme.Activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MediaContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*
+        setContentView(R.layout.activity_media_content);*/
+    }
+}
